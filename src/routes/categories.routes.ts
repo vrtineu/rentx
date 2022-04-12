24f6dad796d1a1948c499cfd6cfcb13451532328
vrtineu <vininro@gmail.com>
2022-04-12 19:17:52 -0300

    docker config

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -11,13 +11,13 @@ const upload = multer({
   dest: './tmp',
 });
 
-categoriesRoutes.post('/', (request, response) =>
-  createCategoryController.handle(request, response)
-);
+categoriesRoutes.post('/', (request, response) => {
+  return createCategoryController.handle(request, response);
+});
 
-categoriesRoutes.get('/', (request, response) =>
-  listCategoriesController.handle(request, response)
-);
+categoriesRoutes.get('/', (request, response) => {
+  return listCategoriesController.handle(request, response);
+});
 
 categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
   return importCategoryController.handle(request, response);
