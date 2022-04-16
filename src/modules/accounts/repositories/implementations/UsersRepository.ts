d03bff8fde8087771b849d428ac58f878f4cd9f4
vrtineu <vininro@gmail.com>
2022-04-15 22:36:22 -0300

    create user implementation

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,37 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  public async create({
+    name,
+    driver_license,
+    email,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      driver_license,
+      email,
+      password,
+    });
+
+    await this.repository.save(user);
+  }
+
+  public async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({ email });
+
+    return user;
+  }
+}
+
+export { UsersRepository };
---
+++ src/modules/accounts/useCase/createUser/CreateUserUseCase.ts
@@ -0,0 +1,37 @@
+import { hash } from 'bcrypt';
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private userRepository: IUsersRepository
+  ) {}
+
+  public async execute({
+    name,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.userRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    await this.userRepository.create({
+      name,
+      email,
+      password: passwordHash,
+      driver_license,
+    });
+  }
+}
+
+export { CreateUserUseCase };
