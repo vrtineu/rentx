78085b6ab072b1a44f657b45fd9ad40c66e6dae7
vrtineu <vininro@gmail.com>
2022-04-17 05:31:36 -0300

    create middleware authenticate and add error handling

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -32,6 +32,12 @@ class UsersRepository implements IUsersRepository {
 
     return user;
   }
+
+  public async findById(id: string): Promise<User> {
+    const user = await this.repository.findOne(id);
+
+    return user;
+  }
 }
 
 export { UsersRepository };
