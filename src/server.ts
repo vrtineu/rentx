012502eadeb0b9a36455bf598a1a015281ea0a45
vrtineu <vininro@gmail.com>
2022-04-23 00:40:50 -0300

    fix imports

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import { AppError } from './errors/AppError';
+import { AppError } from '@errors/AppError';
+
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
