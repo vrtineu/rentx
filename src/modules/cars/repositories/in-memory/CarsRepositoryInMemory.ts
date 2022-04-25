9ba426096371db91c22a2c48804a140bc837f3b0
vrtineu <vininro@gmail.com>
2022-04-25 08:46:56 -0300

    create car use case

---
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -0,0 +1,40 @@
+import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+
+import { ICarsRepository } from '../ICarsRepository';
+
+class CarsRepositoryInMemory implements ICarsRepository {
+  cars: Car[] = [];
+
+  public async create({
+    name,
+    brand,
+    category_id,
+    daily_rate,
+    description,
+    fine_amount,
+    license_plate,
+  }: ICreateCarDTO): Promise<Car> {
+    const car = new Car();
+
+    Object.assign(car, {
+      name,
+      brand,
+      category_id,
+      daily_rate,
+      description,
+      fine_amount,
+      license_plate,
+    });
+
+    this.cars.push(car);
+
+    return car;
+  }
+
+  public async findByLicensePlate(license_plate: string): Promise<Car> {
+    return this.cars.find((car) => car.license_plate === license_plate);
+  }
+}
+
+export { CarsRepositoryInMemory };
