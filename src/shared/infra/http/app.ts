5a078ab1df48e69f9f74a3776dcee9b47321f6c4
vrtineu <vininro@gmail.com>
2022-06-01 21:38:50 -0300

    feat: implementing integration tests

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
+
+import { AppError } from '@shared/errors/AppError';
+import createConnection from '@shared/infra/typeorm';
+
+import swaggerFile from '../../../swagger.json';
+import { router } from './routes';
+
+import '@shared/container';
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, _: Request, response: Response, _next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message}`,
+  });
+});
+
+export { app };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,37 +1,3 @@
-import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-import swaggerUi from 'swagger-ui-express';
-import swaggerFile from 'swagger.json';
-
-import { AppError } from '@shared/errors/AppError';
-import createConnection from '@shared/infra/typeorm';
-
-import { router } from './routes';
-
-import '@shared/container';
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use(router);
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((err: Error, _: Request, response: Response, _next: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      message: err.message,
-    });
-  }
-
-  return response.status(500).json({
-    status: 'error',
-    message: `Internal server error - ${err.message}`,
-  });
-});
+import { app } from './app';
 
 app.listen(3333, () => console.log('Server is running!'));
