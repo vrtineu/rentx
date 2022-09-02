d5ec440fa782c92e8df43e71eba3f7170cb3a25e
vrtineu <vininro@gmail.com>
2022-09-02 13:54:41 -0300

    fix: module resolver config

--- babel.config.js
+++ babel.config.js
@@ -9,11 +9,11 @@ module.exports = {
       'module-resolver',
       {
         alias: {
-          '@modules': ['./src/modules'],
-          '@config': ['./src/config'],
-          '@shared': ['./src/shared'],
-          '@errors': ['./src/errors'],
-          '@utils': ['./src/utils'],
+          '@modules': './src/modules',
+          '@config': './src/config',
+          '@shared': './src/shared',
+          '@errors': './src/errors',
+          '@utils': './src/utils',
         },
       },
     ],
