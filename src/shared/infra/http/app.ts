f54fc1a9cde889ba99f4a21afd56ca39a07492e0
vrtineu <vininro@gmail.com>
2022-09-29 01:22:23 -0300

    chore: add rate limiter as middleware to intercept too many requests

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 import cors from 'cors';
-import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
 
 import upload from '@config/upload';
@@ -10,12 +10,15 @@ import { AppError } from '@shared/errors/AppError';
 import createConnection from '@shared/infra/typeorm';
 
 import swaggerFile from '../../../swagger.json';
+import { rateLimiter } from './middlewares/rateLimiter';
 import { router } from './routes';
 
 import '@shared/container';
 
 createConnection();
 const app = express();
+
+app.use(rateLimiter);
 
 app.use(express.json());
 
